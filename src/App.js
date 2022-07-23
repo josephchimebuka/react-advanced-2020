6229557c5724fbc6b5b50a586dbd088076ce5d7d
josephchimebuka <chimebujoseph@gmail.com>
2022-07-23 05:08:04 +0100

    fetched data from url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from '../src/tutorial/2-useEffect/setup/1-useEffect-basics'
+import Setup from '../src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js'
 function App() {
   return (
     <div className='container'>
